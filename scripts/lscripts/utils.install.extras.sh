50623584a4cdfb1d9e28049f2129e42d87f2b31f
mangalbhaskar <mangalbhaskar@gmail.com>
2019-12-22 02:36:20 +0530

    extra utils added

---
+++ scripts/lscripts/utils.install.extras.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+##----------------------------------------------------------
+### Install utilities and softwares
+## Tested on Ubuntu 16.04 LTS, Ubuntu 18.04 LTS
+##----------------------------------------------------------
+
+sudo -E apt -q -y install redshift
