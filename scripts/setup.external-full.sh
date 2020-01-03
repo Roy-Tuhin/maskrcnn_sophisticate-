8aa53251828ddbf794e7250980ad63b7b995753c
nikhilbv <nikhil.bangale@gmail.com>
2020-01-03 20:01:35 +0530

    full external setup script added

--- scripts/setup.external-full.sh
+++ scripts/setup.external-full.sh
@@ -10,3 +10,15 @@ git clone https://github.com/mapbox/mapbox-vision-android.git
 git clone https://github.com/mangalbhaskar/Mask_RCNN.git
 git clone https://github.com/gustavz/Mobile_Mask_RCNN.git
 git clone https://github.com/nikhilbv/TuSimple-lane-classes.git
+git clone https://github.com/ucbdrive/bdd-data.git
+git clone https://github.com/nikhilbv/Cascade-LD.git
+git clone https://github.com/cardwing/Codes-for-Lane-Detection.git
+git clone https://github.com/pjreddie/darknet
+git clone https://github.com/zhengshoujian/darknet-yolo-segmentation.git
+# git clone https://github.com/facebookresearch/detectron2.git //github.com/saqibmobin/detectron2.git
+git clone https://github.com/mangalbhaskar/aimldl.git
+git clone https://github.com/nikhilbv/simple-keras-rest-api.git
+git clone https://github.com/ShuLiu1993/PANet.git
+git clone https://github.com/XingangPan/seg_label_generate.git
+git clone https://github.com/nikhilbv/tusimple-benchmark.git
+git clone https://github.com/SeokjuLee/VPGNet.git
