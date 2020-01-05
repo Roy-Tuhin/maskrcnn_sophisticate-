64030c7399b978d09f3873af2596264f949f740c
mangalbhaskar <mangalbhaskar@gmail.com>
2020-01-06 03:39:41 +0530

    meshroom error in depth map stucked; cuda.select

--- scripts/lscripts/meshroom.start.sh
+++ scripts/lscripts/meshroom.start.sh
@@ -37,6 +37,7 @@ function meshroom_start() {
   local ALICEVISION_HOME_SRC=/codehub/external/AliceVision
   local TRAINEDVOCABULARYTREEDATA_HOME=/codehub/external/trainedVocabularyTreeData
 
+  # local LD_LIBRARY_PATH=/usr/lib/jvm/java-8-openjdk-amd64/jre/lib/amd64/server
   local PATH=${ALICEVISION_HOME}/bin:$PATH
   local LD_LIBRARY_PATH=${ALICEVISION_HOME}/lib:$LD_LIBRARY_PATH
   local ALICEVISION_SENSOR_DB=${ALICEVISION_HOME_SRC}/src/aliceVision/sensorDB/cameraSensors.db
@@ -54,11 +55,16 @@ function meshroom_start() {
 
   # PYTHONPATH=$PWD
 
-  cd ${MESHROOM_HOME}
-  python meshroom/ui
+  # cd ${MESHROOM_HOME}
+  # python meshroom/ui
 
   ## command line pipeline
   ## python bin/meshroom_photogrammetry --input INPUT_IMAGES_FOLDER --output OUTPUT_FOLDER
+
+  unset LC_CTYPE
+  export LANG=C
+  aliceVision_depthMapEstimation  --input "/tmp/MeshroomCache/StructureFromMotion/d6fc69cf8aa6c04b22449f06cbaee554259e3a59/sfm.abc" --imagesFolder "/tmp/MeshroomCache/PrepareDenseScene/ffbb8fedb25995b4a243f52e5536522857764249" --downscale 8 --minViewAngle 2.0 --maxViewAngle 70.0 --sgmMaxTCams 10 --sgmWSH 4 --sgmGammaC 5.5 --sgmGammaP 8.0 --refineMaxTCams 6 --refineNSamplesHalf 150 --refineNDepthsToRefine 31 --refineNiters 100 --refineWSH 3 --refineSigma 15 --refineGammaC 15.5 --refineGammaP 8.0 --refineUseTcOrRcPixSize False --exportIntermediateResults False --nbGPUs 1 --verboseLevel debug --output "/tmp/MeshroomCache/DepthMap/f4962135bfcdde3e2c99fb50bdc8d54b88854bac" --rangeStart 0 --rangeSize 3
+
 
 }
 
