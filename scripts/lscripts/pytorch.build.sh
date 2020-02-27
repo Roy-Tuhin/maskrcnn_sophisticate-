0d63e72f0fb19e8401847c941b0fd374b9938c92
saqibmobin <saqib96mobin@gmail.com>
2020-02-27 17:33:42 +0530

    minor changes

--- scripts/lscripts/pytorch.build.sh
+++ scripts/lscripts/pytorch.build.sh
@@ -7,24 +7,32 @@ url="https://repo.anaconda.com/archive/Anaconda3-2019.10-Linux-x86_64.sh"
 filename=$(echo ${url} | cut -d "/" -f 5)
 
 
-echo wget ${url} -P ${dir}
-wget ${url} -P ${dir}
+# echo wget ${url} -P ${dir}
+# wget ${url} -P ${dir}
 
-echo source ${dir}/${filename}
-source ${dir}/${filename}
+# echo chmod +x ${dir}/${filename}
+# chmod +x ${dir}/${filename}
+
+# echo ${dir}/${filename}
+# ${dir}/${filename}
+
+# source ~/.bashrc
 
 ##PyTorch dependencies
 
-conda install numpy ninja pyyaml mkl mkl-include setuptools cmake cffi
-conda install -c pytorch magma-cuda101 # or [magma-cuda92 | magma-cuda100 | magma-cuda101 ] depending on your cuda version
+# conda install numpy ninja pyyaml mkl mkl-include setuptools cmake cffi
+# conda install -c pytorch magma-cuda100 # or [magma-cuda92 | magma-cuda100 | magma-cuda101 ] depending on your cuda version
 
-git clone --recursive https://github.com/pytorch/pytorch /codehub/external
+# git clone --recursive https://github.com/pytorch/pytorch /codehub/external/pytorch
 
 cd /codehub/external/pytorch
 
 ## if you are updating an existing checkout
 # git submodule sync
 # git submodule update --init --recursive
+
+##Run this if installation fails
+python setup.py clean
 
 export CMAKE_PREFIX_PATH=${CONDA_PREFIX:-"$(dirname $(which conda))/../"}
 
