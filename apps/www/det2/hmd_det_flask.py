37de146337002e7e1e9518b558c0867183b57ca5
saqibmobin <saqib96mobin@gmail.com>
2020-03-19 15:25:13 +0530

    detectron2 flask server updates

--- apps/www/det2/hmd_det_flask.py
+++ apps/www/det2/hmd_det_flask.py
@@ -11,11 +11,11 @@ from flask import Flask, request, jsonify, render_template
 sys.path.insert(1, '/codehub/external/detectron2')
 sys.path.insert(2, '/codehub/apps/detectron2')
 
-from detectron2.config import config
-from detectron2.data import MetadataCatalog
-from detectron2.engine import DefaultPredictor
+# from detectron2.config import config
+# from detectron2.data import MetadataCatalog
+# from detectron2.engine import DefaultPredictor
 
-from hmd_detectron2 import convert_output_to_json
+# from hmd_detectron2 import convert_output_to_json
 
 # (model, verbose=1, modelcfg=None, image_name=None, im_non_numpy=None, colors=None, get_mask=False, class_names=None)
 
@@ -24,28 +24,28 @@ def inference(image):
   dataset_name = "inference"
   class_ids = ['signage', 'traffic_light', 'traffic_sign']
   id_map = {v: i for i, v in enumerate(class_ids)}
-  metadata = MetadataCatalog.get(dataset_name).set(thing_classes=class_ids)
-  metadata.thing_dataset_id_to_contiguous_id = id_map
+  # metadata = MetadataCatalog.get(dataset_name).set(thing_classes=class_ids)
+  # metadata.thing_dataset_id_to_contiguous_id = id_map
 
-  # MODEL_WEIGHTS_DIR = "/codehub/apps/detectron2/release"
-  MODEL_WEIGHTS_DIR = "/aimldl-dat/release/vidteq/detectron2"
-  PROD_MODEL_WEIGHT = "model_final.pth"
-  arch = "/codehub/cfg/arch/040320_160140-AIE1-01-detectron2.yml"
+  # # MODEL_WEIGHTS_DIR = "/codehub/apps/detectron2/release"
+  # MODEL_WEIGHTS_DIR = "/aimldl-dat/release/vidteq/detectron2"
+  # PROD_MODEL_WEIGHT = "model_final.pth"
+  # arch = "/codehub/cfg/arch/040320_160140-AIE1-01-detectron2.yml"
 
-  cfg = config.get_cfg()
-  cfg.merge_from_file(arch)
-  cfg.DATASETS.TEST = (dataset_name)
-  cfg.OUTPUT_DIR = MODEL_WEIGHTS_DIR
-  cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, PROD_MODEL_WEIGHT)
+  # cfg = config.get_cfg()
+  # cfg.merge_from_file(arch)
+  # cfg.DATASETS.TEST = (dataset_name)
+  # cfg.OUTPUT_DIR = MODEL_WEIGHTS_DIR
+  # cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, PROD_MODEL_WEIGHT)
 
-  predictor = DefaultPredictor(cfg)
+  # predictor = DefaultPredictor(cfg)
 
-  im = cv2.imread(image)
-  outputs = predictor(im)
+  # im = cv2.imread(image)
+  # outputs = predictor(im)
 
-  jsonres = convert_output_to_json(outputs, image, metadata)
-  print(jsonres)
-  return jsonres
+  # jsonres = convert_output_to_json(outputs, image, metadata)
+  # print(jsonres)
+  # return jsonres
 
 app = Flask(__name__)
 # @app.route("/")
